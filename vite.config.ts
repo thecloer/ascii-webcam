b87d5cb28d7627f43cba6998a52d607ef5464467
thecloer <chltjrrb97@gmail.com>
2024-06-19 00:05:18 +0900

    FIX: baseurl

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,9 @@
-vite.config.ts;
+// vite.config.ts
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/ascii-webcam/', // replace with your repository name
+  build: {
+    outDir: 'dist',
+  },
+});
